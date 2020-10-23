78453fa0ba6322d39f30d5a25e5658df2fd1db4e
Rohan kumbhojkar <63917050+rohandk18@users.noreply.github.com>
2020-10-23 20:03:29 +0530

    Add files via upload

---
+++ Inversion_of_Control_Using_ConfigFile/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package springDemo;
+
+public class BaseBallCoach implements Coach
+{
+     public String getDailyWorkout()
+     {
+    	 return "Spend 30 mins on batting practice";
+     }
+}
---
+++ Inversion_of_Control_Using_ConfigFile/TrackCoach.java
@@ -0,0 +1,12 @@
+package springDemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() 
+	{
+		// TODO Auto-generated method stub
+		return "Run a hard 5k";
+	}
+
+}
