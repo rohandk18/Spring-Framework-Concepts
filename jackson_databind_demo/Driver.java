2b53bfdaa5b72a7044fc1b9a39fbe67032f0a387
Rohan kumbhojkar <63917050+rohandk18@users.noreply.github.com>
2020-10-31 13:19:41 +0530

    Add files via upload

---
+++ jackson_databind_demo/Driver.java
@@ -0,0 +1,36 @@
+package com.luv2code.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+public class Driver {
+
+	public static void main(String[] args) 
+	{
+
+		try
+		{
+			//create object mapper
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			//read JSON file and map/convert it to java POJO:  data/sample-lite.json
+			
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			//print firstName and lastName
+			
+			System.out.println("firstName: "+theStudent.getFirstName()+" lastName: "+theStudent.getLastName());
+			
+		}
+		catch(Exception exc)
+		{
+			exc.printStackTrace();
+		}
+
+	}
+
+}
